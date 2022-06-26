0fc6b92f22338a126668359cd1c27d62e448b9d2
dark-person <chanchiho2008@gmail.com>
2022-06-27 00:23:45 +0800

    Setup Background

---
+++ src/Topbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+function Topbar(){
+	return (
+		<div>
+			<h2 className="text-center topbar">少女前線 後勤模擬器</h2>
+			<br />
+		</div>
+		);
+}
+
+export default Topbar;
