3bc481bf9320de890fd6c7aa4639c935dad1045b
dark-person <chanchiho2008@gmail.com>
2022-07-03 19:02:22 +0800

    Add ChapterMenu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
-import Topbar from './Topbar';
+import Topbar from './Topbar'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {useState} from "react"
+import ChapterMenuCol from './ChapterMenu'
+
+let data = require("./supportData.json")
+
+// Const Array, meaning all chapter is selected
+const AllChapterState = new Array(data.chapterList.length).fill(true)
+
+// Const Array, meaning all chapter is NOT selected
+const emptyChapterState = new Array(data.chapterList.length).fill(false);
 
 function App() {
+  // chapterState: state that used by checkbox
+  const [chapterState, setChapterState] = useState(AllChapterState);
+
+  function handleChapterChange(event, id){
+    let temp = chapterState;
+    temp[id] = event.target.checked;
+    setChapterState([...temp]);
+  }
+
+  function selectAllChapters() {
+    setChapterState([...AllChapterState])
+  }
+
+  function clearAllChapters() {
+    setChapterState([...emptyChapterState])
+  }
+
   return (
     <div className="App">
       <Topbar />
+      <Container className="mx-auto my-auto">
+        <Row className="h-100" xs={1} md={2} lg={3}>
+          <ChapterMenuCol chapterState={chapterState} chapterHandler={handleChapterChange} 
+            selectAllHandler={selectAllChapters} clearAllHandler={clearAllChapters}/>
+        </Row>
+      </Container>
+
     </div>
   );
 }
